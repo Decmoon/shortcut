9e51d9b0294284ac559e182220d97f19917d0c70
decmoon <751229863@qq.com>
2020-09-12 19:54:02 +0800

    code optimized for install

--- src/main/java/com/decmoon/shortcut/ShortcutApplication.java
+++ src/main/java/com/decmoon/shortcut/ShortcutApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableShortcutAspect
-@EnableShortcutLogger
 public class ShortcutApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortcutApplication.class, args);
--- src/test/java/com/decmoon/shortcut/ShortcutApplicationTests.java
+++ src/test/java/com/decmoon/shortcut/ShortcutApplicationTests.java
@@ -1,13 +1,9 @@
 package com.decmoon.shortcut;
 
-import com.decmoon.shortcut.argument.Arguments;
 import com.decmoon.shortcut.core.annotation.EnableShortcutAspect;
-import com.decmoon.shortcut.core.annotation.EnableShortcutLogger;
-import com.decmoon.shortcut.core.log.Logger;
 import com.decmoon.shortcut.function.ArgumentsTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
